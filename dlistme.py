58246366de9e4c729b89520121121bbb6894c460
mahongquan <mahongquan@sina.com>
2014-02-21 13:38:59 +0800

    headtail

--- dlistme.py
+++ dlistme.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+def linkab(a,b):
+    a.next=b#link node
+    b.prev=a    
 class Node(object):
     def __init__(self, value=None):
         self.prev = None
@@ -8,11 +11,11 @@ class Node(object):
 
 class Linkedlist(object):
     def __init__(self):
-        self.first = None
-        self.last = None
+        self.head     = None
+        self.tail     = None
     def __str__(self):
         s=""
-        n=self.first
+        n=self.head    
         if n!=None:
             s=str(n.value)
         n=n.next
@@ -20,32 +23,47 @@ class Linkedlist(object):
             s+="->"+str(n.value)
             n=n.next
         return s
+    def str2(self):
+        s=""
+        n=self.tail    
+        if n!=None:
+            s=str(n.value)
+        n=n.prev
+        while n!=None:
+            s+="->"+str(n.value)
+            n=n.prev
+        return s
+    def insert_before(self,node,newnode):
+        if node==None:
+            return
+        prev=node.prev
+        linkab(prev,newnode)
+        linkab(newnode,node)
     def appendleft(self, node):
-        node.prev=None
-        node.next=None
-        if self.first==None:
-            self.first=node
-            self.last=node
+        if self.head    ==None:
+            self.head    =node
+            self.tail    =node
         else:
-            node.next=self.first
-            self.first.prev=node
-            self.first=node
+            linkab(node,self.head    )
+            self.head    =node
     def appendright(self, node):
-        if self.last==None:
-            self.last=node
-            self.first=node
+        if self.tail    ==None:
+            self.tail    =node
+            self.head    =node
         else:
-            node.prev=self.last
-            self.last.next=node
-            self.last=node
+            linkab(self.tail    ,node)
+            self.tail    =node
     def append(self, node):
         return self.appendright(node)
 def main():
     a=Linkedlist()
     n=Node(1)
     a.appendright(n)
-    n=Node(2)
-    a.appendright(n)
+    n2=Node(2)
+    a.appendright(n2)
+    n=Node(3)
+    a.insert_before(n2,n)
     print a
+    print a.str2()
 main()
 
